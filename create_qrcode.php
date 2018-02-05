a86c0efb377016cc03c451c79e2402a0c7174984
sado <sado.tomoki@no1s.biz>
2018-02-05 14:39:41 +0900

    first commit

---
+++ create_qrcode.php
@@ -0,0 +1,12 @@
+<?php
+
+    createImage('http://www.giants.jp/top.html/', 'giants.jpg');
+    createImage('https://www.amazon.co.jp/dp/B01BHPEC9G', 'amazon.jpg');
+    createImage('http://www.cosme.net/product/product_id/10023860/top', 'cosme.jpg');
+    
+    // googleAPIを利用してサイトのQRコードを作成する
+    function createImage($url, $file_name) {
+        $qrcode_url = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={$url}";
+        $data = file_get_contents($qrcode_url);
+        file_put_contents($file_name, $data);
+    }
