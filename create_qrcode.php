d044e6ea776251daeefa83b9018a37ad59ad8d0c
sado <sado.tomoki@no1s.biz>
2018-02-05 16:23:33 +0900

    ディレクトリ配下に保存するようにする

--- create_qrcode.php
+++ create_qrcode.php
@@ -1,4 +1,8 @@
 <?php
+    // imgディレクトリ作成
+    if (!file_exists('img')) {
+        mkdir('img', 0777);
+    }
 
     createImage('http://www.giants.jp/top.html/', 'giants.jpg');
     createImage('https://www.amazon.co.jp/dp/B01BHPEC9G', 'amazon.jpg');
@@ -8,5 +12,5 @@
     function createImage($url, $file_name) {
         $qrcode_url = "https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl={$url}";
         $data = file_get_contents($qrcode_url);
-        file_put_contents($file_name, $data);
+        file_put_contents("img/{$file_name}", $data);
     }
